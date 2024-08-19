9a4bb26673f559c3aaeed95e75513f790db6fe37
Yuma Kishi <obake2ai@gmail.com>
2024-08-19 16:44:11 +0900

    finish total flow

--- audio_handler.py
+++ audio_handler.py
@@ -14,16 +14,32 @@ class MP3EventHandler(FileSystemEventHandler):
         self.model_imagen = path.PATH_IMAGEN
         self.path_output = path.PATH_OUTPUT
 
+        self.wisper = functions.WhisperModel()
+        self.imagen = functions.ImagenModel(
+                                checkpoint_path=self.model_imagen,
+                                image_size=self.size_imagen,
+                                timesteps=self.timesteps_imagen)
+
     def on_modified(self, event):
         if event.is_directory:
             return
         if event.src_path.endswith(".mp3"):
-            print(f"Updated MP3 file: {event.src_path}")
-            # self.start_whisper()
+            print(f"Updated Audio file: {event.src_path}")
+            detected_text = self.start_whisper(event.src_path)
+            print((f"Detected text: {detected_text}"))
+            start_imagen(detected_text)
 
     def start_whisper(self, audio_path):
-        print ("hoge")
+        transcribe_text = self.wisper.transcribe_audio2text(audio_path)
+        check_txtfile = f"{self.path_output}/whisper_output.txt"
+        with open(check_txtfile, 'w') as f:
+            f.write(transcribe_text)
+        return transcribe_text
 
+    def start_imagen(self, prompt):
+        generated_image = self.imagen.generate_image(prompt)
+        generated_image.save(f"{self.path_output}/imagen_output_{prompt}.png")
+        print (f"Imagen saved: {self.path_output}/imagen_output_{prompt}.png")
 
 
 def watch_folder(folder_path):
