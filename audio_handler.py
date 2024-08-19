514db2f023c57859cc84790e160b0db2d1799fb1
Yuma Kishi <obake2ai@gmail.com>
2024-08-19 16:47:28 +0900

    small fix

--- audio_handler.py
+++ audio_handler.py
@@ -19,6 +19,7 @@ class AudioEventHandler(FileSystemEventHandler):
                                 checkpoint_path=self.model_imagen,
                                 image_size=self.size_imagen,
                                 timesteps=self.timesteps_imagen)
+        print("Initial setup complete...Waiting for audio input...")
 
     def on_modified(self, event):
         if event.is_directory:
