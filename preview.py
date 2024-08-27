ed0ddd5d917b1b1ef97235e73a2644dc63234d08
Yuma Kishi <obake2ai@gmail.com>
2024-08-27 12:23:06 +0900

    update preview

--- preview.py
+++ preview.py
@@ -5,26 +5,29 @@ import matplotlib.pyplot as plt
 from src.config import path
 
 def get_latest_image(folder_path):
+    """フォルダ内の最新の画像ファイルを取得"""
     files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
     if not files:
         return None
     latest_file = max(files, key=os.path.getctime)
     return latest_file
 
-def display_image(image_path):
+def display_image(ax, image_path):
+    """画像を表示"""
     img = Image.open(image_path)
-    plt.imshow(img)
-    plt.axis('off')
-    plt.show(block=False)
-    plt.pause(1/24)
-    plt.close()
+    ax.imshow(img)
+    plt.draw()
+    plt.pause(1/24)  # 1/24秒待機
 
 def main(folder_path):
+    """メインループ"""
+    plt.ion()  # インタラクティブモードをオンにする
+    fig, ax = plt.subplots()
     while True:
         latest_image = get_latest_image(folder_path)
         if latest_image:
-            display_image(latest_image)
-        time.sleep(1/24)
+            display_image(ax, latest_image)
+        time.sleep(1/24)  # 1/24秒ごとに画像を更新
 
 if __name__ == "__main__":
     folder_path = path.PATH_OUTPUT
