0e137c0f0c480f9376af88a04a072f76516e6a4c
Yuma Kishi <obake2ai@gmail.com>
2024-09-01 00:07:17 +0900

    add shell

---
+++ start_containers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Dockerコンテナを開始
+docker start solaris02
+
+# ターミナル1: docker exec -it solaris02 nvidia-smi
+gnome-terminal -- bash -c "docker exec -it solaris02 nvidia-smi; exec bash"
+
+# ターミナル2: docker exec -it solaris02 ls
+gnome-terminal -- bash -c "docker exec -it solaris02 ls; exec bash"
+
+# ターミナル3: docker exec -it solaris02 pwd
+gnome-terminal -- bash -c "docker exec -it solaris02 pwd; exec bash"
+
+# ターミナル4: docker exec -it solaris02 du
+gnome-terminal -- bash -c "docker exec -it solaris02 du; exec bash"
