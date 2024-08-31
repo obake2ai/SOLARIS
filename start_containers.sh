9dc3b610a23bcfdbde37f98677ae3b2994291b8f
Yuma Kishi <obake2ai@gmail.com>
2024-09-01 00:31:18 +0900

    update shell

--- start_containers.sh
+++ start_containers.sh
@@ -3,10 +3,10 @@
 # Dockerコンテナを開始
 docker start solaris02
 
-gnome-terminal -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && python3 audio_handler.py'; exec bash"
-gnome-terminal -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && watch -n 1 nvidia-smi'; exec bash"
+gnome-terminal --geometry=80x24+0+0 -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && python3 audio_handler.py'; exec bash"
+gnome-terminal --geometry=80x24+0+270 -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && watch -n 1 nvidia-smi'; exec bash"
 
 sleep 60
 
-gnome-terminal -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && python3 auto_rec.py'; exec bash"
-gnome-terminal -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && python3 preview.py'; exec bash"
+gnome-terminal --geometry=80x24+0+540 -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && python3 auto_rec.py'; exec bash"
+gnome-terminal --geometry=80x24+0+810 -- bash -c "cd ~/Share/SOLARIS/ && python3 preview.py; exec bash"
