2b3d52382fbfbaca6809f73d067886ed4c13f81b
Yuma Kishi <obake2ai@gmail.com>
2024-09-01 00:16:42 +0900

    update shell

--- start_containers.sh
+++ start_containers.sh
@@ -4,13 +4,13 @@
 docker start solaris02
 
 # ターミナル1: docker exec -it solaris02 nvidia-smi
-gnome-terminal -- bash -c "docker exec -it solaris02 nvidia-smi; exec bash"
+gnome-terminal -- bash -c "docker exec -it solaris02 bash -c 'cd /home/yuma/Share/SOLARIS/ && watch -n 1 nvidia-smi'; exec bash"
 
 # ターミナル2: docker exec -it solaris02 ls
-gnome-terminal -- bash -c "docker exec -it solaris02 ls; exec bash"
+gnome-terminal -- bash -c "docker exec -it solaris02 bash -c 'cd /home/yuma/Share/SOLARIS/ && watch -n 1 nvidia-smi'; exec bash"
 
 # ターミナル3: docker exec -it solaris02 pwd
-gnome-terminal -- bash -c "docker exec -it solaris02 pwd; exec bash"
+gnome-terminal -- bash -c "docker exec -it solaris02 bash -c 'cd /home/yuma/Share/SOLARIS/ && watch -n 1 nvidia-smi'; exec bash"
 
 # ターミナル4: docker exec -it solaris02 du
-gnome-terminal -- bash -c "docker exec -it solaris02 watch -n 1 nvidia-smi; exec bash"
+gnome-terminal -- bash -c "docker exec -it solaris02 bash -c 'cd /home/yuma/Share/SOLARIS/ && watch -n 1 nvidia-smi'; exec bash"
