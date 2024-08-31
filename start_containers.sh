f47b66a19cc23c0a4b72ae761c0397b808a4a3d4
Yuma Kishi <obake2ai@gmail.com>
2024-09-01 00:35:50 +0900

    update shell

--- start_containers.sh
+++ start_containers.sh
@@ -3,17 +3,17 @@
 # Dockerコンテナを開始
 docker start solaris02
 
-# ターミナル1: 画面上部（高さの4分の1）
+# ターミナル1: 左上
 gnome-terminal --geometry=160x54+0+0 -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && python3 audio_handler.py'; exec bash"
 
-# ターミナル2: 画面上から1/4の位置（高さの4分の1）
-gnome-terminal --geometry=160x54+0+540 -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && watch -n 1 nvidia-smi'; exec bash"
+# ターミナル2: 右上
+gnome-terminal --geometry=160x54+1920+0 -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && watch -n 1 nvidia-smi'; exec bash"
 
 # 60秒間待機
 sleep 60
 
-# ターミナル3: 画面上から2/4の位置（高さの4分の1）
+# ターミナル3: 左下
 gnome-terminal --geometry=160x54+0+1080 -- bash -c "docker exec -it solaris02 bash -c 'cd ~/Share/SOLARIS/ && python3 auto_rec.py'; exec bash"
 
-# ターミナル4: 画面上から3/4の位置（高さの4分の1）
-gnome-terminal --geometry=160x54+0+1620 -- bash -c "cd ~/Share/SOLARIS/ && python3 preview.py; exec bash"
+# ターミナル4: 右下
+gnome-terminal --geometry=160x54+1920+1080 -- bash -c "cd ~/Share/SOLARIS/ && python3 preview.py; exec bash"
